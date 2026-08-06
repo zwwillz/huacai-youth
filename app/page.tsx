20cab26349534f50fa791a68f8009ab399e32395
willz <zw.will@outlook.com>
2026-08-06 22:48:14 +0800

    Connect preserved UI to database competition results

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import EventApp from "./event-app";
 import LangfangRankingStatic from "./langfang-ranking-static";
+import LangfangDbEnhancer from "./langfang-db-enhancer";
 import { getPublicSiteData } from "@/db/public";
 import { getPublicRankings } from "@/db/rankings";
+import { getCompetitionMatches } from "@/db/competition-matches";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
@@ -9,6 +11,13 @@ export const revalidate = 0;
 export default async function Home() {
   const data = await getPublicSiteData();
   const langfangEventId = data.stations.find((station) => station.id === "langfang")?.eventId;
-  const rankings = langfangEventId ? await getPublicRankings(langfangEventId) : [];
-  return <><EventApp data={data} /><LangfangRankingStatic rankings={rankings} /></>;
+  const [rankings, competitionMatches] = langfangEventId
+    ? await Promise.all([getPublicRankings(langfangEventId), getCompetitionMatches(langfangEventId)])
+    : [[], []];
+
+  return <>
+    <EventApp data={data} />
+    <LangfangRankingStatic rankings={rankings} />
+    <LangfangDbEnhancer matches={competitionMatches} />
+  </>;
 }
