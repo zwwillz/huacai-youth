b118b763081d1aa1423dfea30266366456b0166f
willz <zw.will@outlook.com>
2026-08-06 17:11:07 +0800

    Show static Langfang ranking data

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import appearance from "./data/appearance.json";
 import EventApp from "./event-app";
+import LangfangRankingStatic from "./langfang-ranking-static";
 
 export default function Home() {
   const matches = appearance.matches.filter((match) => match.group === "少年组");
   const players = Array.from(new Set(matches.flatMap((match) => [match.playerA, match.playerB]).filter(Boolean))).sort((a, b) => a.localeCompare(b, "zh-CN"));
-  return <EventApp data={{ matches, players }} />;
+  return <><EventApp data={{ matches, players }} /><LangfangRankingStatic /></>;
 }
