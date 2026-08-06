ade8b6de122562fdafa51a223deea4abf68ccf39
willz <zw.will@outlook.com>
2026-08-06 22:08:33 +0800

    style: load event management styles in admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,6 @@
 import "./admin.css";
+import "./events/event-management.css";
+import "./events/event-settings-index.css";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return children;
