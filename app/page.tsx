f82e0d38d77e9cf2e654b0c09b5527421eca1c6a
willz <zw.will@outlook.com>
2026-08-06 18:52:42 +0800

    feat: load Langfang rankings from database

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import EventApp from "./event-app";
 import LangfangRankingStatic from "./langfang-ranking-static";
 import { getPublicSiteData } from "@/db/public";
+import { getPublicRankings } from "@/db/rankings";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export default async function Home() {
   const data = await getPublicSiteData();
-  return <><EventApp data={data} /><LangfangRankingStatic /></>;
+  const langfangEventId = data.stations.find((station) => station.id === "langfang")?.eventId;
+  const rankings = langfangEventId ? await getPublicRankings(langfangEventId) : [];
+  return <><EventApp data={data} /><LangfangRankingStatic rankings={rankings} /></>;
 }
