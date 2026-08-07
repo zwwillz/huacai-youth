4731ac90d7e73bb368996f31e5dd96fc7c7a62dc
willz <zw.will@outlook.com>
2026-08-08 04:22:09 +0800

    perf(admin): app/admin/layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,7 +2,11 @@ import "./admin.css";
 import "./admin-workspace-shell.css";
 import "./events/event-management.css";
 import "./events/event-settings-index.css";
+import AdminNavigationFeedback from "./admin-navigation-feedback";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return children;
+  return <>
+    <AdminNavigationFeedback />
+    {children}
+  </>;
 }
