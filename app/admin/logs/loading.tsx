b636f9f89e8211f149edb67f5eba3d2360e344ad
willz <zw.will@outlook.com>
2026-08-11 01:38:39 +0800

    perf: add exact operation log loading view

---
+++ app/admin/logs/loading.tsx
@@ -0,0 +1,7 @@
+import { AuditLogLoadingView } from "./audit-log-view";
+import "../system-admin.css";
+import "./audit-log.css";
+
+export default function LogsLoading() {
+  return <AuditLogLoadingView />;
+}
